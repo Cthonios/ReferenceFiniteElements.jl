e1f59da7077f3015f0e6e29163b69314750280a4
Documenter.jl <documenter@juliadocs.github.io>
2023-06-24 13:59:56 +0000

    build based on 96afa3f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ReferenceFiniteElements","category":"page"},{"location":"#ReferenceFiniteElements","page":"Home","title":"ReferenceFiniteElements","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ReferenceFiniteElements.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ReferenceFiniteElements]","category":"page"}]
+}
