f9172ba542f72ea3515637b83c8cf5b022643b68
Craig M. Hamel <cmhamel32@gmail.com>
2023-09-17 10:56:07 -0400

    updating ext.

--- ext/ReferenceFiniteElementsAdaptExt.jl
+++ ext/ReferenceFiniteElementsAdaptExt.jl
@@ -13,7 +13,8 @@ function Adapt.adapt_structure(
   interior_nodes    = Adapt.adapt_structure(to, re.interior_nodes)
   interpolants      = Adapt.adapt_structure(to, re.interpolants)
 
-  ReferenceFiniteElements.ReferenceFE{Itype, N, D, Ftype, L1, L2, typeof(interpolants)}(
+  ReferenceFiniteElements.ReferenceFE{Itype, N, D, Ftype, L1, L2, typeof(interpolants),
+                                      typeof(nodal_coordinates), typeof(face_nodes), typeof(interior_nodes)}(
     nodal_coordinates, face_nodes, interior_nodes, interpolants
   )
 end
