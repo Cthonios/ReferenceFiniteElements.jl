3dd93ee154c8d71c6db847c1d501c55f2e738c4b
Craig M. Hamel <cmhamel32@gmail.com>
2024-10-28 00:47:58 -0400

    adding a type to facilitate surface interpolation mapping.

--- src/ReferenceFEs.jl
+++ src/ReferenceFEs.jl
@@ -109,6 +109,77 @@ surface_shape_function_values(e::ReferenceFE) = e.surface_interps.vals.N
 surface_normal(e::ReferenceFE, q, f) = surface_quadrature_point(e, q, f)
 surface_normals(e::ReferenceFE, q, f) = surface_quadrature_point(e, q, f)
 
+# TODO figure out where to put this
+struct MappedInterpolants{A, B, C, D}
+  X_q::A
+  N::B
+  ∇N_X::C
+  JxW::D
+end
+
+function MappedInterpolants(e::ReferenceFE, X, q)
+  # unpacks
+  w = quadrature_weight(e, q)
+  N = shape_function_value(e, q)
+  ∇N_ξ = shape_function_gradient(e, q)
+
+  # interpolate coordinates
+  X_q = X * N
+
+  # map shape function gradients
+  J = (X * ∇N_ξ)'
+  J_inv = inv(J)
+  ∇N_X = (J_inv * ∇N_ξ')'
+
+  # JxW
+  JxW = det(J) * w
+
+  return MappedInterpolants(X_q, N, ∇N_X, JxW)
+end
+
+struct MappedSurfaceInterpolants{A, B, C, D}
+  X_q::A
+  N::B
+  JxW::C
+  n::D
+end
+
+# specialize for surface shape functions
+function MappedSurfaceInterpolants(e::ReferenceFE{I, F, E}, X, q::Int, f::Int) where {I, F, E <: AbstractTri}
+  # unpacks
+  w = surface_quadrature_weight(e, q, f)
+  N = surface_shape_function_value(e, q, f)[e.edge_nodes[f]]
+  # ∇N_ξ = surface_shape_function_gradient(e, q, f)[e.edge_nodes[f], :]
+  n = surface_normal(e, q, f)
+
+  # TODO below is not general and only works for things
+  # like tractions maybe?
+
+  # unpack coordinates correctly
+  X_temp = X[:, e.edge_nodes[f]]
+  X_diff = X_temp[:, 2] - X_temp[:, 1]
+
+  det_J = norm(X_diff)
+
+  # interpolate coordinates
+  X_q = X_temp * N
+
+  # # map shape function gradients
+  # J = (X_temp * ∇N_ξ)'
+  # J_inv = inv(J)
+  # ∇N_X = (J_inv * ∇N_ξ')'
+
+  # JxW
+  JxW = det_J * w
+
+  # TODO below incorrect. Not giving correct gradient
+  # or normal
+  @show N
+  return MappedSurfaceInterpolants(X_q, N, JxW, n)
+end
+
+
+# deprecate below
 # other methods for working in physical space
 function mapping_jacobian(e::ReferenceFE, X, q)
   J = (X' * shape_function_gradient(e, q))'
@@ -144,6 +215,7 @@ end
 # maybe something like
 # f(X, N, ∇N, ∇∇N, args...)
 
+# TODO need to update below to use MappedInterpolants
 function integrate(f, e::ReferenceFE, X, args...)
   val = zero(eltype(X))
   for q in 1:num_quadrature_points(e)
